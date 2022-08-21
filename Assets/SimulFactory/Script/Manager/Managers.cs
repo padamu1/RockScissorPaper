0b83e3931275dc91f81dd537fbfa85cc484b304e
김선오 <padamu1@naver.com>
2022-08-21 16:47:15 +0900

    씬 추가

---
+++ Assets/SimulFactory/Script/Manager/Managers.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace SimulFactory.Manager
+{
+    public class Managers : MonoSingleton<Managers>
+    {
+        private void Awake()
+        {
+            Init();
+        }
+        private void Init()
+        {
+            EventManager.GetInstance();
+            WorldManager.GetInstance();
+            ObjectPoolManager.GetInstance();
+        }
+    }
+
+}
