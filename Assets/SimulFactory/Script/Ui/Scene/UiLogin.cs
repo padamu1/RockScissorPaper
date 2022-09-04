7c1134f830c5fb2e7cc4350a9447a8c0c2b04de1
김선오 <padamu1@naver.com>
2022-09-04 14:06:20 +0900

    로그인 테스트를 위한 이벤트 및 씬 로딩 추가

--- Assets/SimulFactory/Script/Ui/Scene/UiLogin.cs
+++ Assets/SimulFactory/Script/Ui/Scene/UiLogin.cs
@@ -1,22 +1,27 @@
 using SimulFactory.Game;
 using SimulFactory.Manager;
+using SimulFactory.System.Common;
 using SimulFactory.WebSocket;
 using System.Collections;
 using System.Collections.Generic;
 using TMPro;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 using UnityEngine.UI;
 
 public class UiLogin : MonoBehaviour
 {
+    [SerializeField] private GameObject uiPanel;
     [SerializeField] private TMP_InputField idInput;
     [SerializeField] private TMP_InputField nameInput;
+    private bool isLoginClicked = false;
     private void Awake()
     {
         Managers.GetInstance();
     }
     private void Start()
     {
+        uiPanel.gameObject.SetActive(false);
         StartCoroutine(WaitForServer());
     }
     private IEnumerator WaitForServer()
@@ -26,13 +31,30 @@ public class UiLogin : MonoBehaviour
         {
             yield return waitTime;
         }
+        uiPanel.gameObject.SetActive(true);
+        isLoginClicked = false;
     }
     public void LoginButtonClicked()
     {
-        if(idInput.text.Equals(string.Empty) || nameInput.text.Equals(string.Empty))
+        if (isLoginClicked) return;
+        isLoginClicked = true;
+        if (idInput.text.Equals(string.Empty) || nameInput.text.Equals(string.Empty))
         {
             return;
         }
+        EventManager.GetInstance().StartListening((byte)Define.UNITY_EVENT.Login, LoginState);
         C_Login.LoginC(idInput.text,nameInput.text);
+    }
+    private void LoginState(Dictionary<string,object> message)
+    {
+        EventManager.GetInstance().StopListening((byte)Define.UNITY_EVENT.Login, LoginState);
+        if ((bool)message["result"])
+        {
+            SceneManager.LoadScene("Main");
+        }
+        else
+        {
+            isLoginClicked = false;
+        }
     }
 }
