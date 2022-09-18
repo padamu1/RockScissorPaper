5ec8c9173c3707dcc0286dc9354410b4086fbb5a
김선오 <padamu1@naver.com>
2022-09-18 15:51:02 +0900

    매칭 시작 부분 로직 작성

---
+++ Assets/SimulFactory/Script/Game/Event/S_MatchingResponse.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnityEngine;
+
+namespace SimulFactory.Game.Event
+{
+    public class S_MatchingResponse
+    {
+        public static void MatchingResponseS(Dictionary<byte,object> param)
+        {
+            Debug.Log("매칭 시작 성공");
+        }
+    }
+}
---
+++ Assets/SimulFactory/Script/Game/Event/S_StartMatching.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnityEngine;
+
+namespace SimulFactory.Game.Event
+{
+    public class S_StartMatching
+    {
+        public static void StartMatchingS(Dictionary<byte,object> param)
+        {
+            if ((bool)param[0])
+            {
+                Debug.Log("매칭 시작 성공");
+            }
+        }
+    }
+}
