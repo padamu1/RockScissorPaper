0eba8d4f57a5c9c22554704c56a258157acd50ab
Duplicat <dskim9752@gmail.com>
2022-10-16 17:00:10 +0900

    Add AdmobManager

--- Assets/GoogleMobileAdsBanner.cs
+++ Assets/GoogleMobileAdsBanner.cs
@@ -7,53 +7,80 @@ using System;
 
 public class GoogleMobileAdsBanner: MonoBehaviour
 {
-    private InterstitialAd interstitial;
+    private BannerView bannerView;
 
-    private void Start()
+    public void Start()
     {
-        RequestInterstitial();
+        this.RequestBanner();
     }
 
-    private void RequestInterstitial()
+    private void RequestBanner()
     {
+
 #if UNITY_ANDROID
-        string adUnitId = "ca-app-pub-3940256099942544/1033173712";
+        string adUnitId = "ca-app-pub-3940256099942544/6300978111";
 #elif UNITY_IPHONE
-                string adUnitId = "ca-app-pub-3940256099942544/4411468910";
+            string adUnitId = "ca-app-pub-3940256099942544/2934735716";
 #else
-                string adUnitId = "unexpected_platform";
+            string adUnitId = "unexpected_platform";
 #endif
 
-        // Initialize an InterstitialAd.
-        this.interstitial = new InterstitialAd(adUnitId);
-        // Called when the ad is closed.
-        this.interstitial.OnAdClosed += HandleOnAdClosed;
 
+        AdSize adaptiveSize =
+                AdSize.GetCurrentOrientationAnchoredAdaptiveBannerAdSizeWithWidth(AdSize.FullWidth);
+
+
+        this.bannerView = new BannerView(adUnitId, adaptiveSize, AdPosition.Bottom);
+
+        // Called when an ad request has successfully loaded.
+        this.bannerView.OnAdLoaded += this.HandleOnAdLoaded;
+        // Called when an ad request failed to load.
+        this.bannerView.OnAdFailedToLoad += this.HandleOnAdFailedToLoad;
+        // Called when an ad is clicked.
+        this.bannerView.OnAdOpening += this.HandleOnAdOpened;
+        // Called when the user returned from the app after an ad click.
+        this.bannerView.OnAdClosed += this.HandleOnAdClosed;
 
         // Create an empty ad request.
         AdRequest request = new AdRequest.Builder().Build();
-        // Load the interstitial with the request.
-        this.interstitial.LoadAd(request);
+
+        // Load the banner with the request.
+        this.bannerView.LoadAd(request);
+    }
+
+    public void HandleOnAdLoaded(object sender, EventArgs args)
+    {
+        MonoBehaviour.print("HandleAdLoaded event received");
+    }
+
+    public void HandleOnAdFailedToLoad(object sender, AdFailedToLoadEventArgs args)
+    {
+        MonoBehaviour.print("HandleFailedToReceiveAd event received with message: "
+                            + args.LoadAdError.GetMessage());
+    }
+
+    public void HandleOnAdOpened(object sender, EventArgs args)
+    {
+        MonoBehaviour.print("HandleAdOpened event received");
     }
 
     public void HandleOnAdClosed(object sender, EventArgs args)
     {
-        // 광고를 닫으면 게임 다시 시작하기.
-        SceneManager.LoadScene("PlayScene");
+        MonoBehaviour.print("HandleAdClosed event received");
     }
 
-    private void GameOver()
+
+
+    public void OnDisable()
+
     {
-        if (this.interstitial.IsLoaded())
-        {
-            this.interstitial.Show();
-        }
+        this.bannerView.Hide();
     }
 
-    public void RetryGamePlay()
+    public void OnEnable()
+
     {
-        // RequestInterstitial();
-        GameOver();
-
+        this.bannerView.Show();
     }
+
 }
