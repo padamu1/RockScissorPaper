0cf9005c05d6a69c0a22843de0a161ceb1c7c696
김선오 <padamu1@naver.com>
2022-11-06 14:16:55 +0900

    멀티모드로 실행되게 설정

--- Assets/SimulFactory/Script/Game/Event/C_StartMatching.cs
+++ Assets/SimulFactory/Script/Game/Event/C_StartMatching.cs
@@ -10,7 +10,7 @@ namespace SimulFactory.Game.Event
 {
     public class C_StartMatching
     {
-        public static void StartMatchingC(int type = 0)
+        public static void StartMatchingC(int type = 1)
         {
             Dictionary<byte, object> dic = new Dictionary<byte, object>();
             dic.Add(0, type);
