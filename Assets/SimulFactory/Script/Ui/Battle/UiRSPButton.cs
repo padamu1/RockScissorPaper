0f6303611d6bb21771d93caa070392532d440df0
32163153 <sangkyun625@naver.com>
2023-02-05 14:30:27 +0900

    가위바위보 버튼 클릭시 색변환

--- Assets/SimulFactory/Script/Ui/Battle/UiRSPButton.cs
+++ Assets/SimulFactory/Script/Ui/Battle/UiRSPButton.cs
@@ -3,13 +3,18 @@ using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.EventSystems;
 using UnityEngine.UI;
+using DG.Tweening;
 
 public class UiRSPButton : MonoBehaviour
 {
     [SerializeField] private Button button;
     [SerializeField] private Define.ROCK_SCISSOR_PAPER rspType;
     private Action<Define.ROCK_SCISSOR_PAPER> action;
+
+    public float ButtonColorChangeTime = 0;
+    public Color ButtonChangeColor = Color.gray;
 
     // 현재 버튼의 타입 반환
     public Define.ROCK_SCISSOR_PAPER GetRSPType() => rspType;
@@ -19,5 +24,9 @@ public class UiRSPButton : MonoBehaviour
     public void ButtonClickedAction()
     {
         action?.Invoke(rspType);
+    }
+    public void OnPointerDown(PointerEventData eventData)
+    {
+        gameObject.transform.GetChild(0).GetComponent<Image>().DOColor(ButtonChangeColor, ButtonColorChangeTime);
     }
 }
